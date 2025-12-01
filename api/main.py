315aa354afacd49d480f786c79b35baa5dc8c404
mohammednom3n <mohammednom3n@gmail.com>
2025-12-01 09:42:00 +0530

    Created an api

---
+++ api/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+from src.inference import predict_price
+
+app = FastAPI(title = "House_Price_Pridiction API")
+
+class PrdictionResquest(BaseModel):
+    OverallQual: int
+    GrLivArea: int
+    FirstFlrSF: int = Field(..., alias="1stFlrSF")
+    TotalBsmtSF: int
+    BsmtFinSF1: int
+    LotArea: int
+    GarageCars: int
+    TotRmsAbvGrd: int
+    SecndFlrSF:int = Field(..., alias="2ndFlrSF")
+    YearBuilt: int
+    GarageArea: int
+    FullBath: int
+    OverallCond: int
+    YearRemodAdd: int
+    MSSubClass: int
+
+
+@app.post("/predict")
+def predict(req: PrdictionResquest):
+    data = req.dict(by_alias=True)
+    result = predict_price(data)
+    return {"Esitmated Price": result}
+    
